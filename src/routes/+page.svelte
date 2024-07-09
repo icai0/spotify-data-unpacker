831ba5f9285fb417699c4e25c2cc0112a9fb8373
icai0 <icaiprogramming@gmail.com>
2024-07-08 20:02:15 -0400

    catch wrong json, date inputs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,20 @@
 <script>
 	let fullData = []
 	let boundData = []
+
 	let songInfo = new Map()
 	let artistInfo = new Map()
+
 	let earliestDate
 	let latestDate
 	let startDate
 	let endDate
+
 	let totalTime = 0
+	//total plays is equal to array length
+
+	let startDateInput
+	let endDateInput
 
 	const orders = Object.freeze({
 		PLAYS: 0,
@@ -18,27 +25,36 @@
 	let showPlays = true
 	let showDuration = true
 
+	let displayData = false
+
 	async function onUpload(event) {
 		fullData = []
 		const files = event.target.files
-		
-		for (const file of files){
-			const reader = new FileReader()
-			reader.onload = onReaderLoad
-			reader.readAsText(file)
+		try{
+			for (const file of files){
+				const reader = new FileReader()
+				reader.onload = onReaderLoad
+				reader.readAsText(file)
+			}
+
+			//definitely not the way to do this but I tried the normal ways and gave up
+			await new Promise(resolve => setTimeout(resolve, 1000))
+			
+			fullData.sort((a, b) => a.date.getTime() - b.date.getTime())
+
+			//reset bounds to full data when uploading files
+			const firstTrackDate = fullData[0].date
+			const lastTrackDate = fullData[fullData.length - 1].date
+			startDate = earliestDate = new Date(firstTrackDate.getFullYear(), firstTrackDate.getMonth(), firstTrackDate.getDate(), 0, 0, 0)
+			endDate = latestDate = new Date(lastTrackDate.getFullYear(), lastTrackDate.getMonth(), lastTrackDate.getDate(), 23, 59, 59)
+			startDateInput = dateToInput(startDate)
+			endDateInput = dateToInput(endDate)
+			updateData()
+			displayData = true
+		} catch{
+			alert("Wrong JSON file")
+			displayData = false
 		}
-
-		//definitely not the way to do this but I tried the normal ways and gave up
-		await new Promise(resolve => setTimeout(resolve, 1000))
-
-		fullData.sort((a, b) => a.date.getTime() - b.date.getTime())
-
-		//reset bounds to full data when uploading files
-		const firstTrackDate = fullData[0].date
-		const lastTrackDate = fullData[fullData.length - 1].date
-		startDate = earliestDate = new Date(firstTrackDate.getFullYear(), firstTrackDate.getMonth(), firstTrackDate.getDate(), 0, 0, 0)
-		endDate = latestDate = new Date(lastTrackDate.getFullYear(), lastTrackDate.getMonth(), lastTrackDate.getDate(), 23, 59, 59)
-		updateData()
 	}
 
 	function onReaderLoad(event){
@@ -52,10 +68,16 @@
 	}
 
 	function updateData(){
+		
+		const splitStartDate = startDateInput.split("-")
+		const splitEndDate = endDateInput.split("-")
+		// -1 because for some reason months are 0 - 11
+		startDate = new Date(splitStartDate[0], splitStartDate[1] - 1, splitStartDate[2], 0, 0, 0)
+		endDate = new Date(splitEndDate[0], splitEndDate[1] - 1, splitEndDate[2], 0, 0, 0)
+
 		restrictData()
+
 		currentOrder = orders.DURATION
-		showPlays = true
-		showDuration = true
 		songInfo = new Map()
 		artistInfo = new Map()
 		totalTime = 0
@@ -104,10 +126,24 @@
 			}
 		}
 	}
+
+	function dateToInput(date){
+		return date.toISOString().split("T")[0]
+	}
 </script>
 
 <label for='files'>Choose JSON file(s)</label>
 <input type='file' on:change={onUpload} accept='.json' multiple>
+
+{#if displayData}
+<br>
+<label for='start'>Start date:</label>
+<input type='date' bind:value={startDateInput} on:change={updateData} min={dateToInput(earliestDate)} max={endDateInput}>
+<br>
+<label for='end'>End date:</label>
+<input type='date' bind:value={endDateInput} on:change={updateData} min={startDateInput} max={dateToInput(latestDate)}>
+{/if}
+
 <style>
 	
 </style>
